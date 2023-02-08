d67a202cd6b6738b6fcbb61fcd74605b374b9860
Doeunnkimm <doeunnkimm@gmail.com>
2023-02-08 16:18:28 +0900

    :sparkles: Feat : 댓글 게시 기능

--- src/pages/home/components/Comment/CommentBox.js
+++ src/pages/home/components/Comment/CommentBox.js
@@ -1,13 +1,40 @@
 import * as S from './style';
 import {Text, Image} from '../style';
 import Button from 'components/Button/Button';
+import {useRef, useState} from 'react';
+import myProfile from 'myProfile.json';
 
 function CommentBox({comments}) {
+  const [commentList, setCommentList] = useState(comments);
+  const commentRef = useRef();
+
+  const onClickSubmit = () => {
+    const data = {
+      User: myProfile,
+      content: commentRef.current.value,
+      createdAt: new Date(),
+      id: 'abcdefg',
+      myComment: true,
+    };
+    setCommentList(commentList.unshift(data));
+
+    commentRef.current.value = ''; // clear
+  };
+
+  const toStringByFormatting = (EncodingTime, delimiter = '-') => {
+    const year = EncodingTime.getFullYear();
+    const month = EncodingTime.getMonth() + 1;
+    const day = EncodingTime.getDate();
+    const hour = EncodingTime.getHours();
+    const minute = EncodingTime.getMinutes();
+    return [year, month, day].join(delimiter) + ` ${hour}:${minute}`;
+  };
+
   return (
     <div>
       <S.FlexAlignDiv>
-        <S.Textarea placeholder={'댓글달기..'}></S.Textarea>
-        <Button height={'30px'} width={'10%'}>
+        <S.Textarea placeholder={'댓글달기..'} ref={commentRef}></S.Textarea>
+        <Button height={'30px'} width={'10%'} onClick={onClickSubmit}>
           게시
         </Button>
       </S.FlexAlignDiv>
@@ -21,7 +48,10 @@ function CommentBox({comments}) {
                 width={'30px'}
                 marginRight={'10px'}
               />
-              <span>{comment.id}</span>
+              <span>{comment.User.nick_name}</span>
+              <span style={{marginLeft: '10px', color: 'rgb(160, 160, 160)'}}>
+                {toStringByFormatting(comment.createdAt)}
+              </span>
             </S.FlexAlignDiv>
             <Text>ㄴ {comment.content}</Text>
           </S.Box>
